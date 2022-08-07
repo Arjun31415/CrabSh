5b5c3ff152f234c50f470295faf36dbc37f05325
Arjun31415 <arjunp0710@gmail.com>
2022-08-07 11:02:24 +0530

    refactoring of code

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,34 @@ use std::{
     process::Command,
 };
 extern crate shellexpand;
-
+// function for `cd` command
+fn change_dir(args: std::str::Split<&str>) -> Result<i8, std::io::Error> {
+    let home_dir = dirs::home_dir().unwrap();
+    let new_dir = args.peekable().peek().map_or(home_dir, |x| {
+        // Expand environment variables
+        let temp: &str = &shellexpand::env(*x).unwrap().to_string();
+        // Expand the tilde.
+        let temp: &str = &shellexpand::tilde(temp).to_string();
+        println!("temp path: {}", temp);
+        Path::new(temp).to_path_buf()
+    });
+    let root = Path::new(&new_dir);
+    if let Err(e) = env::set_current_dir(&root) {
+        eprintln!("{}", e);
+        return Err(e);
+    }
+    return Ok(0);
+}
+fn run_command(command: &str, args: std::str::Split<&str>) {
+    let child = Command::new(command).args(args).spawn();
+    // gracefully handle malformed user input
+    match child {
+        Ok(mut child) => {
+            child.wait().unwrap();
+        }
+        Err(e) => eprintln!("No such command found, error: {}", e),
+    };
+}
 fn main_loop() -> Result<u8, String> {
     let prompt = String::from(">");
     loop {
@@ -16,37 +43,19 @@ fn main_loop() -> Result<u8, String> {
         stdout().flush().unwrap();
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
-        let mut full_command = input.trim().split_whitespace();
+        let mut full_command = input.trim().split(" | ");
+
         let command = full_command.next().unwrap();
-        let args = full_command;
+        let args: std::str::Split<&str> = full_command;
         match command {
             "cd" => {
-                let home_dir = dirs::home_dir().unwrap();
-                let new_dir = args.peekable().peek().map_or(home_dir, |x| {
-                    // Expand environment variables
-                    let temp: &str = &shellexpand::env(*x).unwrap().to_string();
-                    // Expand the tilde.
-                    let temp: &str = &shellexpand::tilde(temp).to_string();
-                    println!("temp path: {}", temp);
-                    Path::new(temp).to_path_buf()
-                });
-                let root = Path::new(&new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
-                }
+                change_dir(args).unwrap();
             }
             "exit" => {
                 return Ok(0);
             }
             command => {
-                let child = Command::new(command).args(args).spawn();
-                // gracefully handle malformed user input
-                match child {
-                    Ok(mut child) => {
-                        child.wait().unwrap();
-                    }
-                    Err(e) => eprintln!("No such command found, error: {}", e),
-                };
+                run_command(command, args);
             }
         }
     }
